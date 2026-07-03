61d16785df9fddcbd4265e890176d29cb60529fd
Cogils <pinnx246@gmail.com>
2026-07-03 10:14:04 +0700

    Upload source code files (extracted)

---
+++ baileys-main/lib/index.js
@@ -0,0 +1,37 @@
+import makeWASocket from './Socket/index.js';
+import chalk from "chalk";
+
+console.log(chalk.bold.gray("-----------------------------------------\n"));
+console.log(chalk.bold.cyan(`
+⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣀⡴⢧⣀⠀⠀⣀⣠⠤⠤⠤⠤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠘⠏⢀⡴⠊⠁⠀⠀⠀⠀⠀⠀⠈⠙⠦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣰⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢶⣶⣒⣶⠦⣤⣀⠀
+⠀⠀⠀⠀⠀⠀⢀⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣟⠲⡌⠙⢦⠈⢧
+⠀⠀⠀⣠⢴⡾⢟⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡴⢃⡠⠋⣠⠋
+⠐⠀⠞⣱⠋⢰⠁⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⠤⢖⣋⡥⢖⣫⠔⠋
+⠈⠠⡀⠹⢤⣈⣙⠚⠶⠤⠤⠤⠴⠶⣒⣒⣚⣩⠭⢵⣒⣻⠭⢖⠏⠁⢀⣀
+⠠⠀⠈⠓⠒⠦⠭⠭⠭⣭⠭⠭⠭⠭⠿⠓⠒⠛⠉⠉⠀⠀⣠⠏⠀⠀⠘⠞
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⢤⣀⠀⠀⠀⠀⠀⠀⣀⡤⠞⠁⠀⣰⣆⠀
+⠀⠀⠀⠀⠀⠘⠿⠀⠀⠀⠀⠀⠈⠉⠙⠒⠒⠛⠉⠁⠀⠀⠀⠉⢳⡞⠉
+¤═―— ⎧ 𝐎𝐁𝐈𝐓𝐎 𝐁𝐀𝐈𝐋𝐄𝐘𝐒 ⎭ ⊱―—═¤
+Information:
+Developer: @topsdominic
+Channel: @HattersDmc
+Version: ⊗.⊗
+Status: Baileys Berhasil Tersambung
+`));
+console.log(chalk.bold.gray("--------------------------------------------\n"));
+console.log(chalk.bold.cyan("Follow Our Telegram Channel To See Update Information: t.me/JavaScriptCrazy\n"));;
+
+export * from '../WAProto/index.js';
+export * from './Utils/index.js';
+export * from './Types/index.js';
+export * from './Defaults/index.js';
+export * from './WABinary/index.js';
+export * from './WAM/index.js';
+export * from './WAUSync/index.js';
+export * from './Store/index.js';
+export { makeWASocket };
+export default makeWASocket;
+//# sourceMappingURL=index.js.map
